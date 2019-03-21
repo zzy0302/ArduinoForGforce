70db5ff5621ee4210ed1a41f84fd5a1b7010bb7b
zzy0302 <503652983@qq.com>
2019-03-21 21:53:59 +0800

    qwq

--- demo3/demo3.ino
+++ demo3/demo3.ino
@@ -3,11 +3,16 @@
 #include <Arduino.h>
 #define Timeout 10000
 #define gforceSerial Serial2
+#define MAX 188
+#define MIN 188
 int i = 0;
 float temp_pitch = 0, temp_yaw = 0, temp_roll = 0;
+bool flag=0;
 //min 138 mid 188 max 238
-//pitch +-90 yaw +-180 woll +-180
+//pitch +-90 yaw +-180 roll +-180
 /* returns char count */
+// pitch +-60 yaw +-75
+
 int getChar(unsigned char *data)
 {
   int ret = gforceSerial.read();
@@ -34,6 +39,9 @@ void setup()
     pinMode(i, OUTPUT);
   pinMode(47, INPUT);
   pinMode(48, INPUT);
+  analogWrite(3, 188);
+  analogWrite(4, 188);
+  analogWrite(5, 188);
   // set pin mode to output
   for (i = 0; i < 54; i++)
     digitalWrite(i, LOW);
@@ -43,14 +51,34 @@ void setup()
 }
 
 int get_pitch(int pitch) {
-  return 0.5555 * pitch + 188;
+  float i = 0.8333333 * pitch + 188;
+  if (i > 238)
+    i = MAX;
+  if (i < 138)
+    i = MIN;
+  int p = int(i);
+  return p;
 }
-int get_pitch(int raw) {
-  return 0.5555 * (raw / 2) + 188;
+int get_yaw(int yaw) {
+
+  float i = 0.833333 * yaw + 188;
+  if (i > 238)
+    i = MAX;
+  if (i < 138)
+    i = MIN;
+  int p = int(i);
+  return p;
 }
-int get_pitch(int roll) {
-  return 0.5555 * (roll / 2) + 188;
+int get_roll(int roll) {
+  float i = 0.8333333 * roll + 188;
+  if (i > 238)
+    i = MAX;
+  if (i < 138)
+    i = MIN;
+  int p = int(i);
+  return p;
 }
+
 
 void loop()
 {
@@ -69,57 +97,60 @@ void loop()
           temp_yaw = Euler.yaw;
           temp_roll = Euler.roll;
         }
-        Serial.print("pitch: "); Serial.print(Euler.pitch - temp_pitch);
-        Serial.print(", yaw: "); Serial.print(Euler.yaw - temp_yaw);
-        Serial.print(", roll: "); Serial.print(Euler.roll - temp_roll);
-        Serial.println();
+        //        Serial.print("pitch: "); Serial.print(Euler.pitch);
+        //        Serial.print(", yaw: "); Serial.print(Euler.yaw);
+        //        Serial.print(", roll: "); Serial.print(Euler.roll);
+        //        Serial.println();
         //        Serial.println(get_pitch(Euler.pitch - temp_pitch));
-        analogWrite(3, get_pitch(Euler.pitch - temp_pitch));
-        analogWrite(4, get_pitch(Euler.yaw - temp_yaw));
-        analogWrite(5, get_pitch(Euler.roll - temp_roll));
+        analogWrite(3, get_pitch(Euler.pitch));
+        analogWrite(5, get_yaw(Euler.yaw));
+        analogWrite(6, get_roll(Euler.roll));
         break;
-        //    case GF_Data::GESTURE:
-        //      gesture = gForceData.value.gesture;
-        //      Serial.print("gesture: ");
-        //      Serial.println(gesture);
-        //
-        //      if (gesture == GF_FIST)
-        //      {
-        //        Serial.println("Fist");
-        //      }
-        //      else if (gesture == GF_SPREAD)
-        //      {
-        //        Serial.println("Spread");
-        //      }
-        //      else if (gesture == GF_WAVEIN)
-        //      {
-        //        Serial.println("WaveIn");
-        //      }
-        //      else if (gesture == GF_WAVEOUT)
-        //      {
-        //        Serial.println("WaveOut");
-        //      }
-        //      else if (gesture == GF_PINCH)
-        //      {
-        //        Serial.println("Pinch");
-        //      }
-        //      else if (gesture == GF_SHOOT)
-        //      {
-        //        Serial.println("Shoot");
-        //      }
-        //      else if (gesture == GF_RELEASE)
-        //      {
-        //        Serial.println("Release");
-        //      }
-        //      else if (gesture == GF_UNKNOWN)
-        //      {
-        //        Serial.println("Unknown");
-        //      }
+      case GF_Data::GESTURE:
+        gesture = gForceData.value.gesture;
+        //          Serial.print("gesture: ");
+        //          Serial.println(gesture);
+
+        if (gesture == GF_FIST)
+        {
+          Serial.println("Fist");
+          analogWrite(10, 138+100*flag);
+          flag=flag? 0:1;
+        }
+//        else if (gesture == GF_SPREAD)
+//        {
+//          Serial.println("Spread");
+//        }
+//        else if (gesture == GF_WAVEIN)
+//        {
+//          Serial.println("WaveIn");
+//        }
+//        else if (gesture == GF_WAVEOUT)
+//        {
+//          Serial.println("WaveOut");
+//        }
+//        else if (gesture == GF_PINCH)
+//        {
+//          Serial.println("Pinch");
+//        }
+//        else if (gesture == GF_SHOOT)
+//        {
+//          Serial.println("Shoot");
+//        }
+        else if (gesture == GF_RELEASE)
+        {
+          Serial.println("Release");
+        }
+        else if (gesture == GF_UNKNOWN)
+        {
+          Serial.println("Unknown");
+        }
         break;
       default:
         break;
     }
   }
+
   else
   {
     Serial.print("GFC_GetgForcedata() returned: ");
